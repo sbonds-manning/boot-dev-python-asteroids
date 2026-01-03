e4af38018e153529ebacfe228bd2c69201c7d924
sbonds <sbonds@gmail.com>
2026-01-03 22:43:10 +0000

    CH3 L3 done

--- main.py
+++ main.py
@@ -19,9 +19,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = game_clock.tick(60) / 1000
+        player.update(dt)
+
         player.draw(screen)
         pygame.display.flip()
-        dt = game_clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
