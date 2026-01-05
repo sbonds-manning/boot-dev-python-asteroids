6acfd7c70a46edc19b98a35059b5bf0b9c83a4fe
sbonds <sbonds@gmail.com>
2026-01-05 22:33:48 +0000

    CH4 L3: Shots fire

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -16,9 +17,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = ( updatable, drawable )
     Asteroid.containers = ( asteroids, updatable, drawable )
     AsteroidField.containers = ( updatable )
+    Shot.containers = ( shots, updatable, drawable )
     game_clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
